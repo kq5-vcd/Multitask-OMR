988a5ae6044089a36ee807c271351071d1d65be1
unknown <duy822@ymail.com>
2022-01-02 18:51:21 +0700

    New corpus and result

--- update_data.py
+++ update_data.py
@@ -1,6 +1,7 @@
 import os
+import random
 
-def write_list(filename):
+def write_list(filename, shuffle=False):
     dir_list = []
     for parent, dirnames, filenames in os.walk('Data/' + filename):
         """
@@ -11,9 +12,12 @@ def write_list(filename):
         dir_list = dirnames
         break
 
+    if shuffle:
+        random.shuffle(dir_list) 
+
     f = open("Data/" + filename + "_list.txt", "w")
     for package in dir_list:
         f.write(package + "\n")
     f.close()
 
-write_list('Package')
+write_list('Corpus', True)
